add2f2b88704de18257d57fa4621a95b3b83d13e
Sankalp Johri <sankalp.johri@iiitb.org>
2018-10-06 16:20:23 +0530

    Removed using

--- notification_service/Listeners/RabbitMQListener.cs
+++ notification_service/Listeners/RabbitMQListener.cs
@@ -5,14 +5,14 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using Microsoft.AspNetCore.SignalR.Client;
-using System.Collections.Generic;   
+using System.Collections.Generic;
 using System.Text;
 using notification_service.Utils;
 using RabbitMQ.Client.MessagePatterns;
 
 namespace notification_service.Listeners
 {
-    public class RabbitMQListener: IListener
+    public class RabbitMQListener : IListener
     {
         private HubConnection _connection;
 
@@ -27,30 +27,26 @@ namespace notification_service.Listeners
         {
             Console.WriteLine("Starting RabbitMQ listener");
             var factory = new ConnectionFactory() { HostName = Constants.BASE_URL };
-            using (var connection = factory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "ticket-notification", durable: true, exclusive: false, autoDelete: false, arguments: null);
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                                    {
-                                        Console.WriteLine("Received Message");
-                                        var body = ea.Body;
-                                        var message = Encoding.UTF8.GetString(body);
-                                        Ticket ticket = JsonConvert.DeserializeObject<Ticket>(message);
-                                        Notification notificationMessage = new Notification();
-                                        notificationMessage.ticketId = ticket.TicketId;
-                                        notificationMessage.title = "You have been assigned a ticket";
-                                        notificationMessage.description = ticket.Description;
-                                        notificationMessage.createdOn = DateTime.Now;
-                                        Console.WriteLine("Sending Notification");
-                                        _connection.InvokeAsync("SendNotification", ticket.AgentEmailid, notificationMessage);
-                                        Console.WriteLine(" [x] Sent {0}", JsonConvert.SerializeObject(notificationMessage));
-                                    };
-                    channel.BasicConsume(queue: "ticket-notification", autoAck: true, consumer: consumer);
-                }
-            }
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "ticket-notification", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+                            {
+                                Console.WriteLine("Received Message");
+                                var body = ea.Body;
+                                var message = Encoding.UTF8.GetString(body);
+                                Ticket ticket = JsonConvert.DeserializeObject<Ticket>(message);
+                                Notification notificationMessage = new Notification();
+                                notificationMessage.ticketId = ticket.TicketId;
+                                notificationMessage.title = "You have been assigned a ticket";
+                                notificationMessage.description = ticket.Description;
+                                notificationMessage.createdOn = DateTime.Now;
+                                Console.WriteLine("Sending Notification");
+                                _connection.InvokeAsync("SendNotification", ticket.AgentEmailid, notificationMessage);
+                                Console.WriteLine(" [x] Sent {0}", JsonConvert.SerializeObject(notificationMessage));
+                            };
+            channel.BasicConsume(queue: "ticket-notification", autoAck: true, consumer: consumer);
 
         }
     }
